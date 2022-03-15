8988a609710a995b8b99c19d75f3ca5fd0a6be3b
Niatomi <playervoker@gmail.com>
2022-03-15 14:49:04 +0400

    Delete all methods which works with int's bytes

--- arduino-byte-learn.ino
+++ arduino-byte-learn.ino
@@ -5,55 +5,60 @@ boolean booleanVariable = 16;
 void setup() {
     Serial.begin(9600);
 
+    //Степень числа 2^7
     byte draft = bit(7);
     printByte(draft);
 
+    //Инвертировать биты наоборот
     draft = ~draft;
     printByte(draft);
 
-    Serial.begin(9600);
-    Serial.print("");
-    
+    //Вывод чисел в двоичном виде    
     Serial.println(intNumber, BIN);
     Serial.println(byteNumber, BIN);
     Serial.println(booleanVariable, BIN);
     
-    bitSet(intNumber, 6);
-    Serial.println(intNumber, BIN);
-    
-    bitClear(intNumber, 2);  
-    Serial.println(intNumber, BIN);
-
-    bitWrite(intNumber, 2, 0);  
-    Serial.println(intNumber, BIN);
-
+    //Включает 6-ой бит
     bitSet(byteNumber, 6);
     Serial.println(byteNumber, BIN);
-    
-    bitClear(byteNumber, 2);  
-    Serial.println(byteNumber, BIN);
 
+    //Выключает второй бит во второй позиции
     bitWrite(byteNumber, 2, 0);  
     Serial.println(byteNumber, BIN);
 
+    //Выключает 2-ой бит у byte'а
+    bitClear(byteNumber, 2);  
+    Serial.println(byteNumber, BIN);
+
+    //Выключает 3-ой бит у byte'а
+    bitWrite(byteNumber, 3, 0);  
+    Serial.println(byteNumber, BIN);
+
+    //Включает 6-ой бит у bool переменной
     bitSet(booleanVariable, 6);
     Serial.println(booleanVariable, BIN);
     
+    //Выключает 2-ой бит у bool переменной
     bitClear(booleanVariable, 2);  
     Serial.println(booleanVariable, BIN);
 
+    //Выключает 3-ой бит у bool переменной
     bitWrite(booleanVariable, 3, 0);  
     Serial.println(booleanVariable, BIN);
 
-    byte num = bit(3);
+    //Присваевает переменной num значений 2^3
+    byte num = _BV(3);
     Serial.println(num, BIN);
 
+    //Смещает биты у переменной на три позиции вправо
     intNumber >>= 3;
     Serial.println(intNumber, BIN);
 
+    //Смещает биты у переменной на шесть позиций вправо
     byteNumber >>= 6;
     Serial.println(byteNumber, BIN);
 
+    //Смещает биты у переменной на одну позцию влево
     booleanVariable <<= 1;
     Serial.println(booleanVariable, BIN);
 }
